573433b307345bee1d466baa9f14e34f7cc3c22a
hachimB <boubacarhachim@gmail.com>
2024-05-03 00:35:11 +0100

    test

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -8,19 +8,24 @@ status_codes = {}
 total_size = 0
 line_counter = 0
 
+
 def print_stats():
     print("File size: {}".format(total_size))
     for code in sorted(status_codes.keys()):
         print("{}: {}".format(code, status_codes[code]))
 
+
 def signal_handler(sig, frame):
     print_stats()
     sys.exit(0)
 
+
 signal.signal(signal.SIGINT, signal_handler)
 
 for line in sys.stdin:
-    match = re.match(r'^\S+ - \[.*\] "GET /projects/260 HTTP/1.1" (\d+) (\d+)$', line)
+    match = re.match(
+        r'^\S+ - \[.*\] "GET /projects/260 HTTP/1.1" (\d+) (\d+)$',
+        line)
     if match:
         code = int(match.group(1))
         size = int(match.group(2))
