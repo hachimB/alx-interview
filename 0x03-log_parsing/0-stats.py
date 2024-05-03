53934ca9ad159539e50031e1a99af4e2f6ded0da
hachimB <boubacarhachim@gmail.com>
2024-05-03 01:03:53 +0100

    test

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,13 +1,49 @@
 #!/usr/bin/python3
 """Module documentation"""
+# import sys
+# import re
+# import signal
+
+# status_codes = {}
+# total_size = 0
+# line_counter = 0
+
+
+# def print_stats():
+#     """print_stats"""
+#     print("File size: {}".format(total_size))
+#     for code in sorted(status_codes.keys()):
+#         print("{}: {}".format(code, status_codes[code]))
+
+
+# def signal_handler(sig, frame):
+#     """signal_handler"""
+#     print_stats()
+#     sys.exit(0)
+
+
+# signal.signal(signal.SIGINT, signal_handler)
+
+# for line in sys.stdin:
+#     match = re.match(
+#         r'^\S+ - \[.*\] "GET /projects/260 HTTP/1.1" (\d+) (\d+)$',
+#         line)
+#     if match:
+#         code = int(match.group(1))
+#         size = int(match.group(2))
+#         status_codes[code] = status_codes.get(code, 0) + 1
+#         total_size += size
+#         line_counter += 1
+#         if line_counter % 10 == 0:
+#             print_stats()
+
+# print_stats()
 import sys
-import re
 import signal
 
 status_codes = {}
 total_size = 0
 line_counter = 0
-
 
 def print_stats():
     """print_stats"""
@@ -15,26 +51,28 @@ def print_stats():
     for code in sorted(status_codes.keys()):
         print("{}: {}".format(code, status_codes[code]))
 
-
 def signal_handler(sig, frame):
     """signal_handler"""
     print_stats()
     sys.exit(0)
 
-
 signal.signal(signal.SIGINT, signal_handler)
 
-for line in sys.stdin:
-    match = re.match(
-        r'^\S+ - \[.*\] "GET /projects/260 HTTP/1.1" (\d+) (\d+)$',
-        line)
-    if match:
-        code = int(match.group(1))
-        size = int(match.group(2))
+try:
+    for line in sys.stdin:
+        parts = line.split('"')
+        status_code_and_size = parts[2].split()
+        code = int(status_code_and_size[0])
+        size = int(status_code_and_size[1])
         status_codes[code] = status_codes.get(code, 0) + 1
         total_size += size
         line_counter += 1
         if line_counter % 10 == 0:
             print_stats()
+except KeyboardInterrupt:
+    print_stats()
+    sys.exit(0)
+except IndexError:
+    pass
 
 print_stats()
