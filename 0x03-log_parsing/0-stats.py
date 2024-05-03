931457ae89efc569bb362ad36008e68b6bde1d2a
hachimB <boubacarhachim@gmail.com>
2024-05-03 03:20:04 +0100

    test

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -17,12 +17,33 @@ def print_stats():
             print("{}: {}".format(code, status_codes[code]))
 
 
+# def signal_handler(sig, frame):
+#     """signal_handler"""
+#     print_stats()
+#     sys.stdout.flush()
+#     sys.exit(0)
+
+
+# signal.signal(signal.SIGINT, signal_handler)
+
+# for line in sys.stdin:
+#     match = re.match(
+#         r'^\S+ - \[.*\] "GET /projects/260 HTTP/1.1" (\d+) (\d+)$',
+#         line)
+#     if match:
+#         code = int(match.group(1))
+#         size = int(match.group(2))
+#         status_codes[code] = status_codes.get(code, 0) + 1
+#         total_size += size
+#         line_counter += 1
+#         if line_counter % 10 == 0:
+#             print_stats()
+
+# print_stats()
 def signal_handler(sig, frame):
     """signal_handler"""
     print_stats()
-    sys.stdout.flush()
     sys.exit(0)
-
 
 signal.signal(signal.SIGINT, signal_handler)
 
@@ -33,7 +54,10 @@ for line in sys.stdin:
     if match:
         code = int(match.group(1))
         size = int(match.group(2))
-        status_codes[code] = status_codes.get(code, 0) + 1
+        if code in status_codes:
+            status_codes[code] += 1
+        else:
+            status_codes[code] = 1
         total_size += size
         line_counter += 1
         if line_counter % 10 == 0:
