e06278ac66459ebf4c5a25bcd9675146336f579a
hachimB <boubacarhachim@gmail.com>
2024-05-07 11:40:26 +0100

    UTF-8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -4,10 +4,25 @@
 
 def validUTF8(data):
     """ValidUTF8"""
+    bytes_to_check = 0
     for i in data:
-        binary = bin(i)
-        if binary[2] == '0' or binary[2:5] == '110'\
-                or binary[2:6] == '1110' or binary[2:7] == '11110':
-            return True
+        if i < 0 or i > 255:
+             return False
+        if bytes_to_check == 0:
+            # Use bitwise shift to count the number of leading 1s
+            if (i & 0b10000000) == 0:
+                bytes_to_check = 0
+            elif (i & 0b11100000) == 0b11000000:
+                bytes_to_check = 1
+            elif (i & 0b11110000) == 0b11100000:
+                bytes_to_check = 2
+            elif (i & 0b11111000) == 0b11110000:
+                bytes_to_check = 3
+            else:
+                return False
         else:
-            return False
+            # Use bitwise shift to check if the byte starts with '10'
+            if (i & 0b11000000) != 0b10000000:
+                return False
+            bytes_to_check -= 1
+    return True
