00ecf81e850e89fc5b73a81f67c46493e5c76040
hachimB <boubacarhachim@gmail.com>
2024-05-07 16:26:58 +0100

    UTF-8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -2,59 +2,37 @@
 """Module documentation"""
 
 
-# def validUTF8(data):
-#     # Variable to track the number of expected bytes following the current byte
-#     num_bytes_to_follow = 0
-
-#     # Iterate over each integer in the data set
-#     for byte in data:
-#         # Check if the current byte represents the start of a new UTF-8
-#         # character
-#         if num_bytes_to_follow == 0:
-#             # Count the number of leading 1s in the byte
-#             mask = 1 << 7
-#             while mask & byte:
-#                 num_bytes_to_follow += 1
-#                 mask >>= 1
-
-#             # Invalid start byte if num_bytes_to_follow is 0 or greater than 4
-#             if num_bytes_to_follow == 1 or num_bytes_to_follow > 4:
-#                 return False
-
-#             # If num_bytes_to_follow is 0, the byte is a single ASCII character
-#             if num_bytes_to_follow == 0:
-#                 continue
-
-#         # If num_bytes_to_follow is not 0, the current byte should start with
-#         # "10"
-#         elif byte >> 6 != 0b10:
-#             return False
-
-#         # Decrease the count of expected bytes following the current byte
-#         num_bytes_to_follow -= 1
-
-#     # If num_bytes_to_follow is 0 at the end of the loop, all characters are
-#     # valid
-#     return num_bytes_to_follow == 0
-
 def validUTF8(data):
+    # Variable to track the number of expected bytes following the current byte
     num_bytes_to_follow = 0
 
+    # Iterate over each integer in the data set
     for byte in data:
+        # Check if the current byte represents the start of a new UTF-8
+        # character
         if num_bytes_to_follow == 0:
-            if byte >> 6 == 0b0:  # This is a standalone character
-                continue
-            elif byte >> 5 == 0b110:
-                num_bytes_to_follow = 1
-            elif byte >> 4 == 0b1110:
-                num_bytes_to_follow = 2
-            elif byte >> 3 == 0b11110:
-                num_bytes_to_follow = 3
-            else:  # This byte doesn't represent the start of a valid UTF-8 character
-                return False
-        else:
-            if byte >> 6 != 0b10:  # This is not a valid continuation byte
-                return False
-            num_bytes_to_follow -= 1
+            # Count the number of leading 1s in the byte
+            mask = 1 << 7
+            while mask & byte:
+                num_bytes_to_follow += 1
+                mask >>= 1
 
+            # Invalid start byte if num_bytes_to_follow is 0 or greater than 4
+            if num_bytes_to_follow == 1 or num_bytes_to_follow > 4:
+                return False
+
+            # If num_bytes_to_follow is 0, the byte is a single ASCII character
+            if num_bytes_to_follow == 0:
+                continue
+
+        # If num_bytes_to_follow is not 0, the current byte should start with
+        # "10"
+        elif byte >> 6 != 0b10:
+            return False
+
+        # Decrease the count of expected bytes following the current byte
+        num_bytes_to_follow -= 1
+
+    # If num_bytes_to_follow is 0 at the end of the loop, all characters are
+    # valid
     return num_bytes_to_follow == 0
