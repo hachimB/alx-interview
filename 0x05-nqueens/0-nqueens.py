9039f0acf73cfab63ec32a421451b864fd4fb2fb
hachimB <boubacarhachim@gmail.com>
2024-05-17 11:47:41 +0100

    nqueens

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""nqueens algorithm"""
+import sys
+
+
+if len(sys.argv) != 2:
+    print("Usage: nqueens N")
+    exit(1)
+if not sys.argv[1].isdigit():
+    print("N must be a number")
+    exit(1)
+if int(sys.argv[1]) < 4:
+    print("N must be at least 4")
+    exit(1)
