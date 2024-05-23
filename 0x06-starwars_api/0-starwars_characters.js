2abfc504a25c1d7129295ff3bf21830c80e69e56
hachimB <boubacarhachim@gmail.com>
2024-05-23 16:56:31 +0100

    Star Wars Characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,21 +1,45 @@
 #!/usr/bin/node
+// const request = require('request');
+// const util = require('util');
+
+// const requestPromise = util.promisify(request);
+
+// async function getCharacterNames(url) {
+//   try {
+//     const response = await requestPromise(url);
+//     const characters = JSON.parse(response.body).characters;
+
+//     const characterPromises = characters.map(URL => requestPromise(URL));
+//     const characterResponses = await Promise.all(characterPromises);
+
+//     characterResponses.forEach(characterResponse => {
+//       console.log(JSON.parse(characterResponse.body).name);
+//     });
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+// const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+// getCharacterNames(url);
 const request = require('request');
+const util = require('util');
+
+const requestPromise = util.promisify(request);
+
+async function getCharacterNames (url) {
+  try {
+    const response = await requestPromise(url);
+    const characters = JSON.parse(response.body).characters;
+
+    for (const URL of characters) {
+      const characterResponse = await requestPromise(URL);
+      console.log(JSON.parse(characterResponse.body).name);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
-request(url, (error, response, body) => {
-  if (error) {
-    return console.log(error);
-  }
-  if (response.statusCode === 200) {
-    const characters = JSON.parse(body).characters;
-    characters.forEach(URL => {
-      request(URL, (error, response, body) => {
-        if (error) {
-          return console.log(error);
-        }
-        if (response.statusCode === 200) {
-          console.log(JSON.parse(body).name);
-        }
-      });
-    });
-  }
-});
+getCharacterNames(url);
