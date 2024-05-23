f464ca3b9e774d5f9f6cfa5c564ac5c6a4e591ca
hachimB <boubacarhachim@gmail.com>
2024-05-23 17:04:51 +0100

    Star Wars Characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,27 +1,4 @@
 #!/usr/bin/node
-// const request = require('request');
-// const util = require('util');
-
-// const requestPromise = util.promisify(request);
-
-// async function getCharacterNames(url) {
-//   try {
-//     const response = await requestPromise(url);
-//     const characters = JSON.parse(response.body).characters;
-
-//     const characterPromises = characters.map(URL => requestPromise(URL));
-//     const characterResponses = await Promise.all(characterPromises);
-
-//     characterResponses.forEach(characterResponse => {
-//       console.log(JSON.parse(characterResponse.body).name);
-//     });
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
-
-// const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
-// getCharacterNames(url);
 const request = require('request');
 const util = require('util');
 
@@ -34,7 +11,8 @@ async function getCharacterNames (url) {
 
     for (const URL of characters) {
       const characterResponse = await requestPromise(URL);
-      console.log(JSON.parse(characterResponse.body).name);
+      const names = JSON.parse(characterResponse.body).name;
+      console.log(names);
     }
   } catch (error) {
     console.log(error);
