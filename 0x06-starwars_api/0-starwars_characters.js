03bce4fb9848b5f204b4dca62be3b924ce532aae
hachimB <boubacarhachim@gmail.com>
2024-05-23 16:43:52 +0100

    Star Wars Characters

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,20 @@
+const request = require('request');
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+request(url, (error, response, body) => {
+  if (error) {
+    return console.log(error);
+  }
+  if (response.statusCode === 200) {
+    const characters = JSON.parse(body).characters;
+    characters.forEach(URL => {
+      request(URL, (error, response, body) => {
+        if (error) {
+          return console.log(error);
+        }
+        if (response.statusCode === 200) {
+          console.log(JSON.parse(body).name);
+        }
+      });
+    });
+  }
+});
