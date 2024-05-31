ebcfe6d4a874ac55d9f64e8ebccdcde30f38481a
hachimB <boubacarhachim@gmail.com>
2024-05-31 20:22:56 +0100

    rotate 2D Matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -4,7 +4,4 @@
 
 def rotate_2d_matrix(matrix):
     """Rotate 2D Matrix"""
-    transposed_matrix = [list(row) for row in zip(* matrix)]
-    for i in range(len(transposed_matrix)):
-        for j in range(len(transposed_matrix[i])):
-            matrix[i][j] = transposed_matrix[i][j]
+    matrix[:] = [list(row)[::-1] for row in zip(*matrix)]
