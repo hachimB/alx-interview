18907dc30989237ebe6c82c46a251edb26ccf225
hachimB <boubacarhachim@gmail.com>
2024-06-06 10:09:37 +0100

    making_change(coins algorithm using dynamic programming)

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -12,22 +12,3 @@ def makeChange(coins, total):
         for amount in range(coin, total + 1):
             dp[amount] = min(dp[amount], 1 + dp[amount - coin])
     return -1 if dp[total] == float('inf') else dp[total]
-
-# def makeChange(coins, total):
-#     """makeChange function"""
-#     if total <= 0:
-#         return 0
-
-#     coins.sort(reverse=True)
-#     dp = [float('inf')] * (total + 1)
-#     dp[0] = 0
-
-#     for coin in coins:
-#         if coin > total:
-#             continue
-#         for amount in range(coin, total + 1):
-#             dp[amount] = min(dp[amount], 1 + dp[amount - coin])
-#             if dp[amount] == amount:
-#                 break
-
-#     return -1 if dp[total] == float('inf') else dp[total]
